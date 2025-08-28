486c6e5ed7acf25421e4d6834708311268bdb6fd
Darshan V <darshanv487@gmail.com>
2025-08-28 21:39:08 +0530

    1.2-babsics-of-datatyopes.py: made a list of what all the basic data types

---
+++ 1.2-babsics-of-datatyopes.py
@@ -0,0 +1,40 @@
+# the basics of datatypes
+
+# 1. string
+# 2. int
+# 3. float
+# 4. boolean
+
+# Advanced datatypes
+
+# 1. list
+# 2. tuple
+# 3. set
+# 4. dictionary
+
+# 1. string
+
+a="Darshan"
+print(a)
+print(type(a))
+
+# 2. int
+
+a=10
+print(a)
+print(type(a))
+
+
+# 3. float
+
+a=10.5
+print(a)
+print(type(a))
+
+
+# 4. boolean
+
+a=True
+print(a)
+print(type(a))
+
