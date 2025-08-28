f93ced7be85f4372c45d007201cce7dfeba6d521
Darshan V <darshanv487@gmail.com>
2025-08-28 21:00:08 +0530

    1.0-python-basics.py: added the info about escape charecters and indentation

--- 1.0-python-basics.py
+++ 1.0-python-basics.py
@@ -22,3 +22,17 @@ Name="python"
 
 print(name)
 print(Name)  #here name and Name are different variables
+
+# escape character
+
+print("hello \n world") # new line
+
+print("hello \t world") # tab
+
+print("hello \b world") # backspace
+
+
+# indentation 
+
+if 5>2:
+    print("5 is greater than 2")    
