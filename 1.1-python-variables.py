e0ecf345bc2dca194d72e64db4b1118471d41fe8
Darshan V <darshanv487@gmail.com>
2025-08-28 21:24:42 +0530

    1.1-python-variables.py: learnt about the variables declaration type inference and type casting of the variables

---
+++ 1.1-python-variables.py
@@ -0,0 +1,61 @@
+# variables
+
+a=10
+b="Darshan"
+
+
+print(a)
+print(b)
+
+
+# naming_conventions
+
+# camelCase
+# PascalCase
+# snake_case
+
+# typecasting
+
+a=10
+b=str(a)
+
+print(b)
+print(b, type(b))
+
+# str to int 
+
+a="10"
+b=int(a)
+
+print(b)
+print(b, type(b))
+
+
+# int to float
+
+a=10
+b=float(a)
+
+print(b)
+print(b, type(b))
+
+# float to int
+
+a=10.0
+b=int(a)
+
+print(b)
+print(b, type(b))
+
+# class Person:
+
+#     def describe(self,name,age,height):
+#         self.name=name
+#         self.age=age
+#         self.height=height
+#         print("My name is ",self.name)
+#         print("My age is ",self.age)
+#         print("My height is ",self.height)
+
+# person=Person()
+# person.describe("darshan",25,5.7)
