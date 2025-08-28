fbf38a5a67f072e20e0184760d41ce203c55def4
Darshan V <darshanv487@gmail.com>
2025-08-28 21:33:50 +0530

    1.1-python-variables.py: added the info about the input attribute

--- 1.1-python-variables.py
+++ 1.1-python-variables.py
@@ -47,6 +47,28 @@ b=int(a)
 print(b)
 print(b, type(b))
 
+
+
+a=int(input("Enter the number: "))
+
+print(a)
+print(type(a))
+
+
+num1=int(input("Enter the number: "))
+num2=int(input("Enter the number: "))
+
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+
+print("Addition of ",num1," and ",num2," is ",add)
+print("Subtraction of ",num1," and ",num2," is ",sub)
+print("Multiplication of ",num1," and ",num2," is ",mul)
+print("Division of ",num1," and ",num2," is ",div)
+print("Mod of ",num1," and ",num2," is ",mod)
 # class Person:
 
 #     def describe(self,name,age,height):
