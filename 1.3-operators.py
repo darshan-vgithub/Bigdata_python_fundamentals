a190da470a53cd62c51a3ebf7711acdcffb58e82
Darshan V <darshanv487@gmail.com>
2025-09-05 10:34:34 +0530

    1.3-operators.py: adding the info about all the operators

---
+++ 1.3-operators.py
@@ -0,0 +1,24 @@
+# arthemetic operators
+
+a=10
+b=5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+# compariosn operators
+
+a=10
+b=5
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
