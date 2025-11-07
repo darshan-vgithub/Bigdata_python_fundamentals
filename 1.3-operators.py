99bfed2fea5309a1032e1524bc991d4daf56c91d
Darshan.V <darshanv487@gmail.com>
2025-11-07 20:04:51 +0000

    done with the operators

--- 1.3-operators.py
+++ 1.3-operators.py
@@ -22,3 +22,30 @@ print(a>b)
 print(a<b)
 print(a>=b)
 print(a<=b)
+
+
+# Logical operators.................................
+
+a=True
+b=False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+
+
+
+
+# Simple calculator..............
+
+a=10
+b=5
+
+print("Addition",a+b)
+print("Subtraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)   
+print("Modulo",a%b)
+print("Floor division",a//b)
+print("Exponential",a**b)
